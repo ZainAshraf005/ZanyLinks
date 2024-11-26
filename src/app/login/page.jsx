09c8cb5a06a53782cf5cb51f4436770f20d27804
Zain Ashraf <164740618+ZainAshraf005@users.noreply.github.com>
2024-11-26 17:03:21 +0500

    Update page.jsx

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -38,7 +38,7 @@ const page = () => {
     try {
       e.preventDefault();
       const res = await axios.post(
-        `${process.env.NEXT_PUBLIC_BACKEND_API}/api/auth/login`,
+        `https://zanylinks.up.railway.app/api/auth/login`,
         user,
         {
           withCredentials: true,
