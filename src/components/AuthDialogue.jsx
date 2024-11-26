f9a5feca91d354a042ec8ae0812de0de79ef3668
Zain Ashraf <164740618+ZainAshraf005@users.noreply.github.com>
2024-11-26 17:29:29 +0500

    Update AuthDialogue.jsx

--- src/components/AuthDialogue.jsx
+++ src/components/AuthDialogue.jsx
@@ -26,7 +26,7 @@ const AuthDialogue = ({ dialogue, mode }) => {
 
     try {
       const res = await axios.post(
-        `${process.env.NEXT_PUBLIC_BACKEND_API}/auth/user/delete`,
+        `https://zanylinks.up.railway.app/api/auth/user/delete`,
         user,
         {
           withCredentials: true,
@@ -57,7 +57,7 @@ const AuthDialogue = ({ dialogue, mode }) => {
     };
     try {
       const res = await axios.post(
-        `${process.env.NEXT_PUBLIC_BACKEND_API}/auth/update-password`,
+        `https://zanylinks.up.railway.app/api/auth/update-password`,
         object,
         {
           withCredentials: true,
